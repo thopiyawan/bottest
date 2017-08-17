0dea91bd383286104bf2abddbc6aa05dd619cb14
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-08-17 09:20:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,9 +10,9 @@ if (!is_null($events['events'])) {
  // Loop through each event
  foreach ($events['events'] as $event) {
   // Reply only when message sent is in 'text' format
-  if ($event['type'] == 'message' && $event['message']['type'] == 'sticker') {
+  if ($event['type'] == 'message' && $event['message']['type'] == 'sticker' || $event['type'] == 'message' && $event['message']['type'] == 'text') {
    // Get text sent
-   $text = "hello";
+   $text = $event['message']['text'];
    // Get replyToken
    $replyToken = $event['replyToken'];
 
