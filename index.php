8b561eb9c66b4ec05058130708a5ce5c59e8ba30
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-08-17 14:12:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,52 +1,25 @@
-<?php
-$access_token = 'GKg1wAZ/gjMr6yh3dGmPjuq8HnkDQEZsOdPEfyur3h7JmjdT2JihbEBHL6S4BrLnHCuu0Cv2fSbvwv0/xZqYw+TEjmmqW2mjC5NB9BcVGguZq3CIHX+Vt+fvPcNwtcT2ER0LLVXSwhNN4aVJT0Q08QdB04t89/1O/w1cDnyilFU=';
 
-// Get POST body content
-$content = file_get_contents('php://input');
+ <?php
+ define('LINE_API',"https://notify-api.line.me/api/notify");
+define('LINE_TOKEN','ไม่บอก');
 
+function notify_message($message){
 
-$data = json_decode($json,true);
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
- // Loop through each event
- foreach ($events['events'] as $event) {
-  // Reply only when message sent is in 'text' format
-  if ($event['type'] == 'message' && $event['message']['type'] == 'sticker' || $event['type'] == 'message' && $event['message']['type'] == 'text') {
-   // Get text sent
-//    $text = $event['template'];
-   $text = "hello world!";
-   
-   // Get replyToken
-   $replyToken = $event['replyToken'];
-
-   // Build message to reply back
-   $messages = [
-    'type' => 'text',
-    'text' => $text
-   ];
-
-   // Make a POST Request to Messaging API to reply to sender
-   $url = 'https://api.line.me/v2/bot/message/reply';
-   $data = [
-    'replyToken' => $replyToken,
-    'messages' => [$messages],
-   ];
-   $post = json_encode($data);
-   $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-   $ch = curl_init($url);
-   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-   curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-   curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-   $result = curl_exec($ch);
-   curl_close($ch);
-
-   echo $result . "\r\n";
-  }
- }
+    $queryData = array('message' => $message);
+    $queryData = http_build_query($queryData,'','&');
+    $headerOptions = array(
+        'http'=>array(
+            'method'=>'POST',
+            'header'=> "Content-Type: application/x-www-form-urlencoded\r\n"
+            		  ."Authorization: Bearer ".LINE_TOKEN."\r\n"
+                      ."Content-Length: ".strlen($queryData)."\r\n",
+            'content' => $queryData
+        )
+    );
+    $context = stream_context_create($headerOptions);
+    $result = file_get_contents(LINE_API,FALSE,$context);
+    $res = json_decode($result);
+	return $res;
 }
-echo "OK";
+$res = notify_message('hello');
+var_dump($res); ?>
