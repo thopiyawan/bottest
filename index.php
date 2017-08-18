2c33f190b05aee0d8d9de242315959e7e3237f13
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-08-18 13:39:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -21,23 +21,7 @@ if (!is_null($events['events'])) {
         'type' => 'text',
         'text' => $text
       ];
-     // Make a POST Request to Messaging API to reply to sender
-         $url = 'https://api.line.me/v2/bot/message/reply';
-         $data = [
-          'replyToken' => $replyToken,
-          'messages' => [$messages],
-         ];
-         $post = json_encode($data);
-         $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-         $ch = curl_init($url);
-         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-         curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-         $result = curl_exec($ch);
-         curl_close($ch);
-         echo $result . "\r\n";
+     fc($replyToken, $messages);
   }else{
     $replyToken = $event['replyToken'];
     $text = "ว่าไงนะ";
@@ -45,7 +29,14 @@ if (!is_null($events['events'])) {
         'type' => 'text',
         'text' => $text
       ];
-     // Make a POST Request to Messaging API to reply to sender
+    fc($replyToken, $messages);
+  }
+
+  
+ }
+}
+function fc( $replyToken, $messages) {
+  // Make a POST Request to Messaging API to reply to sender
          $url = 'https://api.line.me/v2/bot/message/reply';
          $data = [
           'replyToken' => $replyToken,
@@ -62,11 +53,7 @@ if (!is_null($events['events'])) {
          $result = curl_exec($ch);
          curl_close($ch);
          echo $result . "\r\n";
-  
-  }
-
-  
- }
+ 
 }
 echo "OK"; 
 
