356da6220dfafcb5aa399e6a145549a5e22e469e
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-08-20 12:12:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -55,21 +55,21 @@ if (!is_null($events['events'])) {
       "title"=> "Menu",
       "text"=> "Please select",
       "actions"=> [
-          {
+          [
             "type"=> "postback",
             "label"=> "Buy",
             "data"=> "action=buy&itemid=123"
-          },
-          {
+          ],
+          [
             "type"=> "postback",
             "label"=> "Add to cart",
             "data"=> "action=add&itemid=123"
-          },
-          {
+           ],
+          [
             "type"=> "uri",
             "label"=> "View detail",
             "uri"=> "http://example.com/page/123"
-          }
+          ]
       ]
 
     ];  
