be0fcc4bc764cc77f3d69def58e443926bffb7a4
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-08-21 09:12:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if (!is_null($events['events'])) {
  // Loop through each event
  foreach ($events['events'] as $event) {
   // Reply only when message sent is in 'text' format
-  if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $event['message']['text'] == "สวัสดี") {
+  if (strpos($_msg, 'คำนวณ') !== false)) {
       $replyToken = $event['replyToken'];
       $text = "hello world!";
       $messages = [
