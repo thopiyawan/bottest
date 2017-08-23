62c04c2c8aa8515a866247d82326fcd93dd7cdf5
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-08-23 14:08:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -159,12 +159,28 @@ if (!is_null($events['events'])) {
          echo "<br>Content: ";
          echo $json->responseData->results[$x]->content;
          echo "<br><br>";
-      $messages = [
-              'type' => 'text',
-              'text' =>  $json
-            ];
+ 
      
          }
+    
+     
+       $url = 'https://api.line.me/v2/bot/message/reply';
+         $data = [
+          'replyToken' => $replyToken,
+          'messages' => [$json],
+         ];
+         error_log(json_encode($data));
+         $post = json_encode($data);
+         $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+         $ch = curl_init($url);
+         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+         curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+         $result = curl_exec($ch);
+         curl_close($ch);
+         echo $result . "\r\n";
   
  }else{
     $replyToken = $event['replyToken'];
