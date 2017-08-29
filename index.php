f8811feba3cba43a4262bba09a17651ef15cff0c
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-08-29 14:29:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -177,20 +177,59 @@ if (!is_null($events['events'])) {
   $data = json_encode($data); 
     
   
-   $messages = [
-        'type' => 'template',
-        'altText' => 'this is a carousel template',
-        'template' => [
-            'type' => 'carousel',
-            'columns' => [
+   $messages = {
+  "type": "template",
+  "altText": "this is a carousel template",
+  "template": {
+      "type": "carousel",
+      "columns": [
           {
             "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
             "title": "this is menu",
-            "text": "description"
-          }]
-            
-        ]
-    ];
+            "text": "description",
+            "actions": [
+                {
+                    "type": "postback",
+                    "label": "Buy",
+                    "data": "action=buy&itemid=111"
+                },
+                {
+                    "type": "postback",
+                    "label": "Add to cart",
+                    "data": "action=add&itemid=111"
+                },
+                {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://example.com/page/111"
+                }
+            ]
+          },
+          {
+            "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+            "title": "this is menu",
+            "text": "description",
+            "actions": [
+                {
+                    "type": "postback",
+                    "label": "Buy",
+                    "data": "action=buy&itemid=222"
+                },
+                {
+                    "type": "postback",
+                    "label": "Add to cart",
+                    "data": "action=add&itemid=222"
+                },
+                {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://example.com/page/222"
+                }
+            ]
+          }
+      ]
+  }
+}
    //$re = $events['kind'];
     
   
