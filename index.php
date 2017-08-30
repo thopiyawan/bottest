0cb8c2f2088962427099bc33a53114552998b0a6
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-08-30 16:20:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -201,10 +201,10 @@ $action = [];
     }
    
 
-              $val= array(  "type"=> "uri",
-                            "label" => $title,
-                            "uri" => $link
-                               );
+              $val= ["type"=> "uri",
+                     "label" => $title,
+                     "uri" => $link
+                    ];
     
    $messages = [
         'type' => 'template',
