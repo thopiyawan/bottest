35a40cbcde53f389b8f2b3966eeb015b34765c3f
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-08-30 09:44:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -197,17 +197,44 @@ $action = [];
 //        array_push($data,$val);
     }
    
-   $messages = [
+   $messages =  [
         'type' => 'template',
         'altText' => 'this is a carousel template',
         'template' => [
             'type' => 'carousel',
             'columns' => [
                [
-                    'title' => 'hello',
+                    'title' => 'this is menu',
                     'text' => 'description',
-                    'actions' => $action
-               ]
+                    'actions' => [
+                        [
+                            'type' => 'postback',
+                            'label' => 'buy',
+                            'data' => 'value'
+                        ],
+                        [
+                            'type' => 'uri',
+                            'label' => 'add to cart',
+                            'uri' => 'http://clinic.e-kuchikomi.info/'
+                        ]
+                    ]
+                ],
+                [
+                    'title' => 'this is menu',
+                    'text' => 'description',
+                    'actions' => [
+                        [
+                            'type' => 'postback',
+                            'label' => 'buy',
+                            'data' => 'value'
+                        ],
+                        [
+                            'type' => 'uri',
+                            'label' => 'add to catrt',
+                            'uri' => 'https://jobikai.com/'
+                        ]
+                    ]
+                ],
             ]
         ]
     ];
