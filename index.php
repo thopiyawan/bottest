47520f5adb7e722e7c74144a317d869b37bb563a
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-09-01 13:31:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -197,20 +197,33 @@ $action = [];
 //        array_push($data,$val);
     }
    
-   $messages = [
-        'type' => 'template',
-        'altText' => 'this is a carousel template',
-        'template' => [
-            'type' => 'carousel',
-            'columns' => [
-              {
-                "type": "uri",
-                "label": "View detail",
-                "uri": "http://example.com/page/123"
-              }
-           ]
-        ]
-    ];
+   $messages = {
+  "type": "template",
+  "altText": "this is a buttons template",
+  "template": {
+      "type": "buttons",
+      "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+      "title": "Menu",
+      "text": "Please select",
+      "actions": [
+          {
+            "type": "postback",
+            "label": "Buy",
+            "data": "action=buy&itemid=123"
+          },
+          {
+            "type": "postback",
+            "label": "Add to cart",
+            "data": "action=add&itemid=123"
+          },
+          {
+            "type": "uri",
+            "label": "View detail",
+            "uri": "http://example.com/page/123"
+          }
+      ]
+  }
+}
    /*
 $messages = array('type' => 'template',
             'altText' => 'this is a carousel template',
