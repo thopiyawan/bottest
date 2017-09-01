e0d96ef9972692970378e867b595ef2bda35769c
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-09-01 14:14:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -223,6 +223,7 @@ $val = [];
         'template' => [
             'type' => 'carousel',
             'columns' => [
+            for($j=0;$j<=3;$j++){
                [
                     'title' => 'this is menu',
                     'text' => 'description',
@@ -239,6 +240,7 @@ $val = [];
                         ]
                     ]
                 ],
+            }
                 [
                     'title' => 'this is menu',
                     'text' => 'description',
