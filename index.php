b914e596ec24c8649a4a3e5db725c29c4672b61c
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-09-06 09:46:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -218,7 +218,7 @@ if (!is_null($events['events'])) {
                             'uri' => $events['items'][$i]['link']
                         ]
                     ]
-			}       ],
+              ]},
             ]
         ]
     ];
