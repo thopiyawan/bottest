da398f18117ffe3bac11f31a27345c27034544e5
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-09-12 09:44:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -201,7 +201,7 @@ if (!is_null($events['events'])) {
 // 	  ]]];
 	  
        for ($i = 0 ; $i<5 ; $i++){
-$me = [[
+$me[$i] = [[
                     'title' => $events['items'][$i]['title'],
                     'text' => 'description',
                     'actions' => [
@@ -220,19 +220,19 @@ $me = [[
 
   
    
-
+       }
 
    $messages = [
         'type' => 'template',
         'altText' => 'this is a carousel template',
         'template' => [
             'type' => 'carousel',
-            'columns' => $i
+            'columns' => $me
         ]
     ];
   
    
-   }
+   
 //////////////////////////////////////// TEST DATA ////////////////////////////////////////////////
             
 //     $me = [];
