1cdd983c811ec66647098c788d0594b51a4d1b02
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-09-13 15:30:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -140,13 +140,13 @@ if (!is_null($events['events'])) {
 	  
 $db_connection = pg_connect("host=ec2-54-163-233-201.compute-1.amazonaws.com dbname=db user=njppbbukwreesq password=
 c6b890bd6e0dccc4a5db3308869ba5e2735fe0e5df7a3f0de6f114cc24752e04");	        
-$result = pg_query($db_connection, """CREATE TABLE "dchdrsngrf50pd"."MyGuests" (
+$result = pg_query($db_connection, "CREATE TABLE 'dchdrsngrf50pd'.'MyGuests' (
 			id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
 			firstname VARCHAR(30) NOT NULL,
 			lastname VARCHAR(30) NOT NULL,
 			email VARCHAR(50),
 			reg_date TIMESTAMP
-			)""");
+			)");
 	  
 	  
    
