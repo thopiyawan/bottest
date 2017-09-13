de5171f0ba9e4c0009342e7a398522937c88ea3d
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-09-13 14:14:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
       
-// include('db.php');  
+include('db.php');  
 $access_token = 'GKg1wAZ/gjMr6yh3dGmPjuq8HnkDQEZsOdPEfyur3h7JmjdT2JihbEBHL6S4BrLnHCuu0Cv2fSbvwv0/xZqYw+TEjmmqW2mjC5NB9BcVGguZq3CIHX+Vt+fvPcNwtcT2ER0LLVXSwhNN4aVJT0Q08QdB04t89/1O/w1cDnyilFU=';
 // $conn = new mysqli($servername, $username, $password, $dbname);
 
