2a5246c5fa47c12c473f3f60d84db2d7de824bd2
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-09-14 22:24:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -147,7 +147,7 @@ $dbconn = pg_pconnect($conn_string);
 // VALUES ($height,$date)";
 
 	  
-$sql="INSERT INTO history(userid,date_history,width,height)VALUES ($replyToken, NOW() ,$width,$height)";
+$sql="INSERT INTO history(userid,date_history,width,height)VALUES ('2',NOW(),$width,$height)";
 pg_exec($dbconn, $sql) or die(pg_errormessage()); 	  
 	 
 // if( mysql_select_db($Dbname)){
