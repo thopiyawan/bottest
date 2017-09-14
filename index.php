b09e5b32b63575edb60f40bf01bd6dcbc81e2d0b
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-09-14 16:01:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -142,14 +142,11 @@ $user = $events['events'][0]['source']['userId'];
 $conn_string = "host=ec2-54-163-233-201.compute-1.amazonaws.com port=5432 dbname=dchdrsngrf50pd user=njppbbukwreesq password=c6b890bd6e0dccc4a5db3308869ba5e2735fe0e5df7a3f0de6f114cc24752e04";
 $dbconn = pg_pconnect($conn_string);
 
-$sql="INSERT INTO history (historyid, userid)
+$sql="INSERT INTO test (historyid, userid)
 VALUES ('fdvz1', 'fdvf' )";
 	  
 pg_exec($dbconn, $sql) or die(pg_errormessage()); 	  
 	  
-	  
-  historyid character varying(100),
-  userid character varying(100)	  
 	  
 	  
 	  
