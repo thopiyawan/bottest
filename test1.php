24996bd5cced464aa1bd4c9ee19b11a5fc04eb1f
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-09-14 13:12:53 +0700

    Update test1.php

--- test1.php
+++ test1.php
@@ -1,10 +1,11 @@
 <?php  
 $conn_string = "host=ec2-54-163-233-201.compute-1.amazonaws.com port=5432 dbname=dchdrsngrf50pd user=njppbbukwreesq password=c6b890bd6e0dccc4a5db3308869ba5e2735fe0e5df7a3f0de6f114cc24752e04";
 $dbconn = pg_connect($conn_string);
-// $sql = "INSERT INTO book (id, name,width,height) VALUES ('001', '50.00', '160.00')";
+
 if (!$dbconn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+$sql = "INSERT INTO data_test.history (id, name,width,height) VALUES ('001', '50.00', '160.00')";
 // $sql = "CREATE TABLE History(History_ID INT NOT NULL AUTO_INCREMENT,
 // userID VARCHAR (100),
 // date_history DATE,
@@ -15,7 +16,7 @@ if (!$dbconn) {
     
 //     $dbconn2 = pg_connect($sql);
     
- echo " successfully";
+ //echo " successfully";
 
 //$ResId = pg_exec("INSERT INTO data_test.history (id, name,width,height) VALUES ('001', '50.00', '160.00')", $dbconn);
 ?>
