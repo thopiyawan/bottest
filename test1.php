4bf36de5d86525afffb303c105d2b43bad837dce
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-09-14 09:52:03 +0700

    Update test1.php

--- test1.php
+++ test1.php
@@ -1,5 +1,5 @@
 <?php  
-$conn_string = "host=ec2-23-21-220-167.compute-1.amazonaws.com port=5432 dbname=dh3dj7jtq6jct user=kywyvkvocykcqg password=76902c76ba27fc88dbde51ca9c2e7d67af1ec06ffd14ba80853acf8e748c4a47";
+$conn_string = "ec2-54-163-233-201.compute-1.amazonaws.com port=5432 dbname=dchdrsngrf50pd user=njppbbukwreesq password=c6b890bd6e0dccc4a5db3308869ba5e2735fe0e5df7a3f0de6f114cc24752e04";
 $dbconn = pg_connect($conn_string);
 if (!$dbconn) {
     die("Connection failed: " . mysqli_connect_error());
