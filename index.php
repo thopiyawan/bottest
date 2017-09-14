67abc07994c01de8f53abb53fece827fb3c7847e
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-09-14 20:48:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -143,15 +143,13 @@ $user = $events['events'][0]['source']['userId'];
 $conn_string = "host=ec2-54-163-233-201.compute-1.amazonaws.com port=5432 dbname=dchdrsngrf50pd user=njppbbukwreesq password=c6b890bd6e0dccc4a5db3308869ba5e2735fe0e5df7a3f0de6f114cc24752e04";
 $dbconn = pg_pconnect($conn_string);
 
-$sql="INSERT INTO test (historyid, userid)
-VALUES ($height,$date)";
+// $sql="INSERT INTO test (historyid, userid)
+// VALUES ($height,$date)";
+
 	  
+$sql="INSERT INTO test (userid,date_history,width,height)VALUES ($user,$date,$width,$height)";
 pg_exec($dbconn, $sql) or die(pg_errormessage()); 	  
-	  
-	  
-	  
-	  
-	  
+	 
 // if( mysql_select_db($Dbname)){
 //           $replyToken = $event['replyToken'];
 //       $text = "บันทึกสำเร็จ";
@@ -177,15 +175,6 @@ pg_exec($dbconn, $sql) or die(pg_errormessage());
     $width  = str_replace("","",$pieces[1]);
 //********ใส่ 5 ค่าลง array********//
 	  
-	        
-$result = pg_query($db_connection, "CREATE TABLE MyGuests (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-reg_date TIMESTAMP
-)");
-
 	  
 	  
     $result = $width/($height*$height);
