cf6b2a7c6257290c9995f485da02770c37632402
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-09-14 09:38:31 +0700

    Update test1.php

--- test1.php
+++ test1.php
@@ -3,7 +3,7 @@ $servername = "ec2-54-163-233-201.compute-1.amazonaws.com" ;
 $username = "njppbbukwreesq" ;
 $password = "dc6b890bd6e0dccc4a5db3308869ba5e2735fe0e5df7a3f0de6f114cc24752e04" ;
 $dbname = "dchdrsngrf50pd" ;
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = pg_connect($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
