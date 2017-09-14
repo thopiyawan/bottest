acdf8605f0a642ba19f295ee8da3432e56865140
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-09-14 10:37:56 +0700

    Update test1.php

--- test1.php
+++ test1.php
@@ -16,15 +16,5 @@ $dbconn = pg_connect($conn_string);
     
 //  echo " successfully";
 
-$result = pg_query($dbconn, "CREATE TABLE MyGuests (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-reg_date TIMESTAMP
-)");
- if  (!$result) {
-    echo "query did not execute";
-  }
-
+$ResId = pg_exec ("CREATE TABLE MyGuests", $dbconn);
 ?>
