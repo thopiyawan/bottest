3cfbc11297d25683e45ebb913d2ebce979bffdb7
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-09-14 09:57:04 +0700

    Update test1.php

--- test1.php
+++ test1.php
@@ -1,19 +1,47 @@
 <?php  
 $conn_string = "host=ec2-54-163-233-201.compute-1.amazonaws.com port=5432 dbname=dchdrsngrf50pd user=njppbbukwreesq password=c6b890bd6e0dccc4a5db3308869ba5e2735fe0e5df7a3f0de6f114cc24752e04";
 $dbconn = pg_connect($conn_string);
-if (!$dbconn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-$sql = "CREATE TABLE History(History_ID INT NOT NULL AUTO_INCREMENT,
-userID VARCHAR (100),
-date_history DATE,
-width DOUBLE,
-height DOUBLE,
-PRIMARY KEY(History_ID)
-)";
+// if (!$dbconn) {
+//     die("Connection failed: " . mysqli_connect_error());
+// }
+// $sql = "CREATE TABLE History(History_ID INT NOT NULL AUTO_INCREMENT,
+// userID VARCHAR (100),
+// date_history DATE,
+// width DOUBLE,
+// height DOUBLE,
+// PRIMARY KEY(History_ID)
+// )";
     
-    $dbconn2 = pg_connect($sql);
+//     $dbconn2 = pg_connect($sql);
     
- echo " successfully";
+//  echo " successfully";
 
+
+   if(!$dbconn) {
+      echo "Error : Unable to open database\n";
+   } else {
+      echo "Opened database successfully\n";
+   }
+
+   $sql =<<<EOF
+      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+      VALUES (1, 'Paul', 32, 'California', 20000.00 );
+
+      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+      VALUES (2, 'Allen', 25, 'Texas', 15000.00 );
+
+      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+      VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );
+
+      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+      VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );
+EOF;
+
+   $ret = pg_query($db, $sql);
+   if(!$ret) {
+      echo pg_last_error($db);
+   } else {
+      echo "Records created successfully\n";
+   }
+   pg_close($db);
 ?>
