89363b673f83519512f7e9808eba93df0a4f85e6
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-09-14 13:59:13 +0700

    Update test1.php

--- test1.php
+++ test1.php
@@ -6,17 +6,17 @@ if (!$dbconn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// $sql = "CREATE TABLE History(History_ID INT NOT NULL AUTO_INCREMENT,
-// userID VARCHAR (100),
-// date_history DATE,
-// width DOUBLE,
-// height DOUBLE,
-// PRIMARY KEY(History_ID)
-// )";
+$sql = "CREATE TABLE History(History_ID INT NOT NULL AUTO_INCREMENT,
+userID VARCHAR (100),
+date_history DATE,
+width DOUBLE,
+height DOUBLE,
+PRIMARY KEY(History_ID)
+)";
     
 //     $dbconn2 = pg_connect($sql);
 //     
  //echo " successfully";
-
-$ResId = pg_exec("INSERT INTO data_test.history (id, name,width,height) VALUES ('001', '50.00', '160.00')", $dbconn);
+pg_exec($dbconn, $sql) or die(pg_errormessage()); 
+// $ResId = pg_exec("INSERT INTO data_test.history (id, name,width,height) VALUES ('001', '50.00', '160.00')", $dbconn);
 ?>
