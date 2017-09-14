fa7c6013f94f0b232402c8b7fac65157395e550d
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-09-14 09:35:16 +0700

    Update test1.php

--- test1.php
+++ test1.php
@@ -1,21 +1,27 @@
 <?php  
-$hostname = "ec2-54-163-233-201.compute-1.amazonaws.com" ;
-$Dbuser = "njppbbukwreesq" ;
-$Dbpwd = "dc6b890bd6e0dccc4a5db3308869ba5e2735fe0e5df7a3f0de6f114cc24752e04" ;
-$Dbname = "dchdrsngrf50pd" ;
-mysql_connect($hostname, $Dbuser, $Dbpwd  ); 
-if( mysql_select_db($Dbname)){
-          $replyToken = $event['replyToken'];
-      $text = "บันทึกสำเร็จ";
-      $messages = [
-        'type' => 'text',
-        'text' => $text
-      ];
-}else{ 
-	 $replyToken = $event['replyToken'];
-      $text = "บันทึกไม่สำเร็จ";
-      $messages = [
-        'type' => 'text',
-        'text' => $text
-      ]; }
+$servername = "ec2-54-163-233-201.compute-1.amazonaws.com" ;
+$username = "njppbbukwreesq" ;
+$password = "dc6b890bd6e0dccc4a5db3308869ba5e2735fe0e5df7a3f0de6f114cc24752e04" ;
+$dbname = "dchdrsngrf50pd" ;
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+// sql to create table
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
 ?>
