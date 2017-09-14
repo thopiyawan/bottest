80f52b5174813d793b2de5efb6a6b7a44db3f350
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-09-14 09:50:18 +0700

    Update test1.php

--- test1.php
+++ test1.php
@@ -1,37 +1,19 @@
 <?php  
-// $servername = "ec2-54-163-233-201.compute-1.amazonaws.com" ;
-// $username = "njppbbukwreesq" ;
-// $password = "dc6b890bd6e0dccc4a5db3308869ba5e2735fe0e5df7a3f0de6f114cc24752e04" ;
-// $dbname = "dchdrsngrf50pd" ;
-// $conn = pg_connect($servername, $username, $password, $dbname);
+$conn_string = "host=ec2-23-21-220-167.compute-1.amazonaws.com port=5432 dbname=dh3dj7jtq6jct user=kywyvkvocykcqg password=76902c76ba27fc88dbde51ca9c2e7d67af1ec06ffd14ba80853acf8e748c4a47";
+$dbconn = pg_connect($conn_string);
+if (!$dbconn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "CREATE TABLE History(History_ID INT NOT NULL AUTO_INCREMENT,
+userID VARCHAR (100),
+date_history DATE,
+width DOUBLE,
+height DOUBLE,
+PRIMARY KEY(History_ID)
+)";
+    
+    $dbconn2 = pg_connect($sql);
+    
+ echo " successfully";
 
-
-$conn_string = "host=ec2-54-163-233-201.compute-1.amazonaws.com port=5432 dbname=dchdrsngrf50pd user=njppbbukwreesq password=dc6b890bd6e0dccc4a5db3308869ba5e2735fe0e5df7a3f0de6f114cc24752e04";
-$dbconn4 = pg_connect($conn_string);
-
-  if(!$dbconn4) {
-      echo "Error : Unable to open database\n";
-   } else {
-      echo "Opened database successfully\n";
-   }
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// } 
-
-// // sql to create table
-// $sql = "CREATE TABLE MyGuests (
-// id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-// firstname VARCHAR(30) NOT NULL,
-// lastname VARCHAR(30) NOT NULL,
-// email VARCHAR(50),
-// reg_date TIMESTAMP
-// )";
-
-// if ($conn->query($sql) === TRUE) {
-//     echo "Table MyGuests created successfully";
-// } else {
-//     echo "Error creating table: " . $conn->error;
-// }
-
-// $conn->close();
 ?>
