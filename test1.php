1c9264e28d8daf30b896d431a52f048bf167a5a2
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-09-14 09:29:57 +0700

    Add files via upload

---
+++ test1.php
@@ -0,0 +1,21 @@
+<<?php  
+$hostname = "ec2-54-163-233-201.compute-1.amazonaws.com" ;
+$Dbuser = "njppbbukwreesq" ;
+$Dbpwd = "dc6b890bd6e0dccc4a5db3308869ba5e2735fe0e5df7a3f0de6f114cc24752e04" ;
+$Dbname = "dchdrsngrf50pd" ;
+mysql_connect($hostname, $Dbuser, $Dbpwd  ); 
+if( mysql_select_db($Dbname)){
+          $replyToken = $event['replyToken'];
+      $text = "บันทึกสำเร็จ";
+      $messages = [
+        'type' => 'text',
+        'text' => $text
+      ];
+}else{ 
+	 $replyToken = $event['replyToken'];
+      $text = "บันทึกไม่สำเร็จ";
+      $messages = [
+        'type' => 'text',
+        'text' => $text
+      ]; }
+?>
