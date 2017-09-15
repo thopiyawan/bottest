9641afa8cdff4cb4e69e77e6dc0b53449d02badc
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-09-15 08:38:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -139,7 +139,7 @@ if (!is_null($events['events'])) {
     $width  = str_replace("","",$pieces[1]);
 
 //********ใส่ 5 ค่าลง array********//	
-$user = $events['events'][0]['source']['userId'];
+$user = $events['events'][1]['source']['userId'];
 $conn_string = "host=ec2-54-163-233-201.compute-1.amazonaws.com port=5432 dbname=dchdrsngrf50pd user=njppbbukwreesq password=c6b890bd6e0dccc4a5db3308869ba5e2735fe0e5df7a3f0de6f114cc24752e04";
 $dbconn = pg_pconnect($conn_string);
 
@@ -147,7 +147,7 @@ $dbconn = pg_pconnect($conn_string);
 // VALUES ($height,$date)";
 
 	  
-$sql="INSERT INTO history(userid,date_history,width,height)VALUES ('2',NOW(),$width,$height)";
+$sql="INSERT INTO history(userid,date_history,width,height) VALUES('13', NOW() , $width , $height)";
 pg_exec($dbconn, $sql) or die(pg_errormessage()); 	  
 	 
 // if( mysql_select_db($Dbname)){
