a7edebd83c611c56aed2290f40689078effc2a77
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-09-15 09:49:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -140,6 +140,8 @@ if (!is_null($events['events'])) {
 
 //********ใส่ 5 ค่าลง array********//	
 // $user_id= $events['events'][0]['source']['userId'];
+
+$_msg = $events['events'][0]['message']['text'];
 $conn_string = "host=ec2-54-163-233-201.compute-1.amazonaws.com port=5432 dbname=dchdrsngrf50pd user=njppbbukwreesq password=c6b890bd6e0dccc4a5db3308869ba5e2735fe0e5df7a3f0de6f114cc24752e04";
 $dbconn = pg_pconnect($conn_string);
 
