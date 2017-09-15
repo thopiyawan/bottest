da11c5dc84b7e834469f94a83829c17cd38ead6f
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-09-15 16:13:21 +0700

    Update test1.php

--- test1.php
+++ test1.php
@@ -38,7 +38,7 @@ if (!$dbconn) {
 //*************************
 $sql="CREATE TABLE history (
 historyid  SERIAL,
-user VARCHAR(225), 
+users varchar(225), 
 date_history DATE,
 weight varchar(3),
 height varchar(3),
