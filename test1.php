50b2a694346357f2f8a8a5c8047c8469f5adfdd5
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-09-15 16:17:03 +0700

    Update test1.php

--- test1.php
+++ test1.php
@@ -36,14 +36,14 @@ if (!$dbconn) {
 // )";   
 
 //*************************
-$sql="CREATE TABLE history (
-historyid  SERIAL,
-user_id varchar(225), 
-date_history DATE,
-weight varchar(3),
-height varchar(3),
-PRIMARY KEY(historyid)
-)";   
+// $sql="CREATE TABLE history (
+// historyid  SERIAL,
+// user_id varchar(225), 
+// date_history DATE,
+// weight varchar(3),
+// height varchar(3),
+// PRIMARY KEY(historyid)
+// )";   
 //FOREIGN KEY(userid) REFERENCES users(userid)
 // $sql="CREATE TABLE users (
 // userid varchar(100),
@@ -57,7 +57,7 @@ PRIMARY KEY(historyid)
 // userid varchar(100)
 // )";   
  
-pg_exec($dbconn, $sql) or die(pg_errormessage()); 
+// pg_exec($dbconn, $sql) or die(pg_errormessage()); 
 
 
 
@@ -66,9 +66,9 @@ pg_exec($dbconn, $sql) or die(pg_errormessage());
 
 
 
-// $user = "3hh3";
-// $height = "33";
-// $weight = "344";
-// $sql="INSERT INTO history(userid,date_history,weight,height) VALUES($user,NOW() , $weight, $height )";
-// pg_exec($dbconn, $sql) or die(pg_errormessage());
+$user = "3hh3";
+$height = "33";
+$weight = "344";
+$sql="INSERT INTO history(user_id,date_history,weight,height) VALUES($user,NOW() , $weight, $height )";
+pg_exec($dbconn, $sql) or die(pg_errormessage());
 ?>
