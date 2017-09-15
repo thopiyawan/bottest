17a2f768b8b07c895db714c8aa0ebbf10be5c6e7
thopiyawan <31061645+thopiyawan@users.noreply.github.com>
2017-09-15 15:59:21 +0700

    Update test1.php

--- test1.php
+++ test1.php
@@ -59,7 +59,7 @@ if (!$dbconn) {
 $user = 'U243424324ghjkjhgf';
 $height = '33';
 $weight = '344';
-$sql="INSERT INTO history(userid,date_history,weight,height) VALUES( $user,NOW() , $weight, $height )";
+$sql="INSERT INTO history(userid,date_history,weight,height) VALUES('33',NOW() , $weight, $height )";
 pg_exec($dbconn, $sql) or die(pg_errormessage()); 
 // pg_exec($dbconn, $sql) or die(pg_errormessage()); 
 
