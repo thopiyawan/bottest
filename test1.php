e787ae3037a71c41e3597cd41f659aa77611332a
piyawan <tho.piyawan@gmail.com>
2017-09-19 14:00:44 +0700

    xcz

--- test1.php
+++ test1.php
@@ -5,71 +5,27 @@ if (!$dbconn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// $sql1 ="CREATE TABLE user (
-// user_id varchar(100),
-// name varchar(20),
-// date_of_birth varchar(20), 
-// PRIMARY KEY(user_id)
-// )"; 
 
- $sql="DROP TABLE IF EXISTS history";
- pg_exec($dbconn, $sql) or die(pg_errormessage());
+//////////////////////////////////////////////////////////////////////
+ // $sql="DROP TABLE IF EXISTS history";
+ // pg_exec($dbconn, $sql) or die(pg_errormessage());
+//////////////////////////////////////////////////////////////////////
 
 
 
 
 
-// pg_exec($dbconn, $sql1) or die(pg_errormessage()); 
 
+//*************************
 // $sql="CREATE TABLE history (
-// id_his varchar(5),
-// width varchar(20), 
-// height varchar(20),
-// date_history varchar(20),
-// user_id varchar(100),
-// PRIMARY KEY(id_his),
-// FOREIGN KEY(user_id) REFERENCES user(user_id)
-// )";
-
-// $sql="CREATE TABLE History_1 (
-// HistoryID varchar(100),
-// userID varchar(100), 
+// historyid  SERIAL,
 // date_history DATE,
-// weight INT,
-// height INT,
-// PRIMARY KEY(HistoryID),
-// FOREIGN KEY(userID) REFERENCES user(user_id)
+// user_id text, 
+// weight varchar(3),
+// height varchar(3),
+// PRIMARY KEY(historyid)
 // )";   
-
-//*************************
-$sql="CREATE TABLE history (
-historyid  SERIAL,
-date_history DATE,
-user_id text, 
-weight varchar(3),
-height varchar(3),
-PRIMARY KEY(historyid)
-)";   
-//FOREIGN KEY(userid) REFERENCES users(userid)
-// $sql="CREATE TABLE users (
-// userid varchar(100),
-// name varchar(50),
-// date_of_birth DATE,
-// PRIMARY KEY(userid)
-//)";
-
-// $sql="CREATE TABLE test (
-// historyid varchar(100),
-// userid varchar(100)
-// )";   
- 
-pg_exec($dbconn, $sql) or die(pg_errormessage()); 
-
-
-
-// pg_exec($dbconn, $sql) or die(pg_errormessage()); 
-
-// $weight = "SELECT weight FROM history ";	  
+//**************************
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 // $query = 'select weight from history ';
@@ -81,15 +37,14 @@ pg_exec($dbconn, $sql) or die(pg_errormessage());
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-// $result = pg_query($dbconn, "SELECT * FROM history;");
-// $sql = "INSERT INTO interests (id, interests) VALUES ($1, $2)";
-// pg_prepare('my_query', $sql);
-// pg_execute('my_query', array($id, $int)) or die("Error while inserting.");
-// $height = "33";
-// $weight = "344";
-// $sql="INSERT INTO history(historyid,date_history,user_id,weight,height) VALUES(historyid,NOW(),$user , $weight, $height )";
 
 
-// $cd = pg_exec($dbconn, $sql) or die(pg_errormessage());
+$user = "ffdvfdav34333";
+$height = "33";
+$weight = "344";
+$sql="INSERT INTO history(historyid,date_history,user_id,weight,height) VALUES(historyid,NOW(),$user,$weight,$height )";
+
+
+$cd = pg_exec($dbconn, $sql) or die(pg_errormessage());
 
 ?>
