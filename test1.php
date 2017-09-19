365b17573ed52a001c755dded39ddf1a5c9fd683
piyawan <tho.piyawan@gmail.com>
2017-09-19 10:37:41 +0700

    vs

--- test1.php
+++ test1.php
@@ -65,7 +65,7 @@ if (!$dbconn) {
 
 // $weight = "SELECT weight FROM history ";	  
 
-$result = pg_query($dbconn, "SELECT weight FROM public.history" );
+$result = pg_query($dbconn, "SELECT weight FROM history WHERE weight='60'" );
 $count = pg_fetch_result($result, 'howManyUsersHaveThisName');
 echo $count;
 
