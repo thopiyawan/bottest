393edda494671da32d464a7b78d86501676e77ae
piyawan <tho.piyawan@gmail.com>
2017-09-19 11:24:07 +0700

    h

--- test1.php
+++ test1.php
@@ -64,19 +64,39 @@ if (!$dbconn) {
 // pg_exec($dbconn, $sql) or die(pg_errormessage()); 
 
 // $weight = "SELECT weight FROM history ";	  
-$query = 'select * from history';
+$query = 'select weight from history ';
 
 $result = pg_query($query);
 
 $i = 0;
 
-pg_num_fields($result)
+// while ($i < pg_num_fields($result))
+// {
+// 	$fieldName = pg_field_name($result, $i);
+// 	echo '<td>' . $fieldName . '</td>';
+// 	$i = $i + 1;
+// }
 
-	$fieldName = pg_field_name($result, $i);
-	echo $fieldName;
+// $i = 0;
 
+while ($row = pg_fetch_row($result)) 
+{
+	echo '<tr>';
+	$count = count($row);
+	$y = 0;
+	while ($y < $count)
+	{
+		$c_row = current($row);
+		echo '<td>' . $c_row . '</td>';
+		next($row);
+		$y = $y + 1;
+	}
+	echo '</tr>';
+	$i = $i + 1;
+}
+pg_free_result($result);
 
-
+echo '</table></body></html>';
 
 
 
