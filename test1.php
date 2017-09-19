88ccceef80894608d8febaab2cfcc8056ae89ceb
piyawan <tho.piyawan@gmail.com>
2017-09-19 11:22:36 +0700

    v

--- test1.php
+++ test1.php
@@ -69,34 +69,14 @@ $query = 'select * from history';
 $result = pg_query($query);
 
 $i = 0;
-echo '<html><body><table><tr>';
-while ($i < pg_num_fields($result))
-{
+
+pg_num_fields($result)
+
 	$fieldName = pg_field_name($result, $i);
-	echo '<td>' . $fieldName . '</td>';
-	$i = $i + 1;
-}
-echo '</tr>';
-$i = 0;
+	echo $fieldName;
 
-while ($row = pg_fetch_row($result)) 
-{
-	echo '<tr>';
-	$count = count($row);
-	$y = 0;
-	while ($y < $count)
-	{
-		$c_row = current($row);
-		echo '<td>' . $c_row . '</td>';
-		next($row);
-		$y = $y + 1;
-	}
-	echo '</tr>';
-	$i = $i + 1;
-}
-pg_free_result($result);
 
-echo '</table></body></html>';
+
 
 
 
