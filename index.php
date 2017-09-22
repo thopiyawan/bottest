d4984b436574d997270f74dfda8c9ee4b7694d16
piyawan <tho.piyawan@gmail.com>
2017-09-22 15:48:41 +0700

    fsgf

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ if (!is_null($events['events'])) {
                  $replyToken = $event['replyToken'];
                  $messages = [
                         'type' => 'text',
-                        'text' =>  $user
+                        'text' =>  $question
                       ];
 
                 $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0006','','0007','1',NOW(),NOW())") or die(pg_errormessage());
