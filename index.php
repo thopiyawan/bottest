1a2b1fb815fe521abbeff80b97ffa08e7e026897
piyawan <tho.piyawan@gmail.com>
2017-09-25 09:23:32 +0700

    rea

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $_msg = $events['events'][0]['message']['text'];
 $user = $events['events'][0]['source']['userId'];
 $user_id = pg_escape_string($user);
 
-$check_q = pg_query($dbconn,"SELECT seqcode, sender_id ,updated_at  FROM sequentsteps  WHERE sender_id = $user_id  order by updated_at desc limit 1   ");
+$check_q = pg_query($dbconn,"SELECT seqcode, sender_id ,updated_at  FROM sequentsteps  WHERE sender_id = '{$user_id}'  order by updated_at desc limit 1   ");
                 while ($row = pg_fetch_row($check_q)) {
                   echo $seqcode =  $row[0];
                   echo $sender = $row[2]; 
@@ -29,7 +29,7 @@ if($event['message']['text'] == "5"){
        $replyToken = $event['replyToken'];
                  $messages = [
                         'type' => 'text',
-                        'text' => 'fdv'
+                        'text' => $seqcode
                       ];  
 
 
