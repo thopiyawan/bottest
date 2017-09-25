a08fe97160f9f16eabd1233dfa57ce7d7844057f
piyawan <tho.piyawan@gmail.com>
2017-09-25 12:01:59 +0700

    d

--- index.php
+++ index.php
@@ -117,7 +117,7 @@ if (!is_null($events['events'])) {
             ]
         ]
     ];     
-            $q = pg_exec($dbconn, "INSERT INTO users_data(user_id ,user_age,user_weight, user_height, preg_week )VALUES('{$user_id}',$age,'0','0','0' ") or die(pg_errormessage());
+            $q = pg_exec($dbconn, "INSERT INTO users_data(user_id ,user_age,user_weight, user_height, preg_week )VALUES('{$user_id}',$age,'','','' ") or die(pg_errormessage());
 
 
 
