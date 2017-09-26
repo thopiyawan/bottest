d929d00ef703a4281d93ce8fd03e69fd6518d7d4
piyawan <tho.piyawan@gmail.com>
2017-09-26 13:58:31 +0700

    x

--- index.php
+++ index.php
@@ -117,7 +117,7 @@ if (!is_null($events['events'])) {
         ]
     ];     
 
-      $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0008', $age ,'0009','0',NOW(),NOW())") or die(pg_errormessage());
+      $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0006', $age ,'0007','0',NOW(),NOW())") or die(pg_errormessage());
            
        // $q = pg_exec($dbconn, "INSERT INTO user_data(user_id,user_age,user_weight,user_height,preg_week )VALUES('{$user_id}',$age,'0','0','0') ") or die(pg_errormessage());      
 
