6e1f3dd4ac7466a077ddb829f0e5f23e2e403f12
piyawan <tho.piyawan@gmail.com>
2017-09-26 09:45:31 +0700

    s

--- test1.php
+++ test1.php
@@ -102,7 +102,7 @@ $sql="CREATE TABLE history_preg(
 his_preg_id SERIAL,
 user_id  varchar(225),
 his_preg_week  varchar(2),
-his_preg_weight varchar(3),
+his_preg_weight varchar(3)
  -- PRIMARY KEY(his_preg_id),
  -- FOREIGN KEY (his_preg_week) REFERENCES Pregnancy_week_data(week_preg),
  -- FOREIGN KEY (user_id) REFERENCES users_data(user_id)
