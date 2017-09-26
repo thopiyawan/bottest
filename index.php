638ac94b63bab0de37f7b1ba448d496dc11a4a17
piyawan <tho.piyawan@gmail.com>
2017-09-26 08:50:03 +0700

    ddd

--- index.php
+++ index.php
@@ -185,7 +185,7 @@ if (!is_null($events['events'])) {
             ]
         ]
     ];   
-   $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0008',  $am,'0009','0',NOW(),NOW())") or die(pg_errormessage());
+   $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0008',  $a,'0009','0',NOW(),NOW())") or die(pg_errormessage());
     
   }elseif ($event['message']['text'] == "อายุครรภ์ถูกต้อง" ) {
                  $replyToken = $event['replyToken'];
