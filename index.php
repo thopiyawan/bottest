c012ef8daaecdb0e2d28a288235a550268a8fd5b
piyawan <tho.piyawan@gmail.com>
2017-09-27 14:31:59 +0700

    s

--- index.php
+++ index.php
@@ -157,55 +157,58 @@ if (!is_null($events['events'])) {
         
 
     if (is_numeric($month) !== false &&is_numeric($date) !== false && strlen($date) == 2 && strlen($month) == 2  ) {
-                 $replyToken = $event['replyToken'];
+        if($date<31 && $date >=0 && $month <12 && $month>=0){
+           if($date>$date_today){
+               $d_pre = $date - $date_today;
+                if($d_pre>=7){
+                   $m_pre = ($month_today - $month)*4;
+                   $w_pre =  $d_pre/7;
+                   $w_pre = number_format($w_pre);
+                  /////คำตอบ/////
+                   $re_date_pre =  $d_pre%7;
+                   $re_date_pre = number_format($re_date_pre);
+                   $re_week_pre = $m_pre+$w_pre;
+
+                }else{
+                   $re_date_pre = $date - $date_today;
+                   $re_week_pre = ($month_today - $month)*4; 
+                }
+
+           }else{
+               $d_pre = $date_today - $date;
+                if($d_pre>=7){
+                   $m_pre = ($month_today - $month)*4;
+                   $w_pre =  $d_pre/7;
+                   $w_pre = number_format($w_pre);
+                  /////คำตอบ/////
+                   $re_date_pre =  $d_pre%7;
+                   $re_date_pre = number_format($re_date_pre);
+                   $re_week_pre = $m_pre+$w_pre;
+
+                }else{
+                   $re_date_pre = $date_today- $date;
+                   $re_week_pre = ($month_today - $month)*4; 
+                }
+
+           }
+        
+        }else{
+          $replyToken = $event['replyToken'];
                  $messages = [
                         'type' => 'text',
-                        'text' => '1'
-                      ];  
+                        'text' => 'กรุณาพิมพ์ใหม่'
+                      ]; 
 
+        }
     } else {
   
-                  $replyToken = $event['replyToken'];
+                $replyToken = $event['replyToken'];
                  $messages = [
                         'type' => 'text',
-                        'text' => '2'
+                        'text' => 'กรุณาพิมพ์ใหม่ตามนี้ 17 02(วันที่ เดือน)'
                       ];  
 
-
     }   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     // if (is_numeric( $date) !== false && strlen($date) == 2 && strlen($month) == 2  ){      
@@ -249,30 +252,30 @@ if (!is_null($events['events'])) {
 //             $date_pre = $date_today-$date;
 //         }
 //     $month_pre = ($month_today-$month)*4 ;
-//     $age_pre = 'คุณมีอายุครรภ์'. $month_pre.'สัปดาห์'. $date_pre .'วัน' ;
+    $age_pre = 'คุณมีอายุครรภ์'. $re_week_pre.'สัปดาห์'. $re_date_pre .'วัน' ;
 
 
-    // $replyToken = $event['replyToken'];
-    // $messages = [
-    //     'type' => 'template',
-    //     'altText' => 'this is a confirm template',
-    //     'template' => [
-    //         'type' => 'confirm',
-    //         'text' =>  $age_pre ,
-    //         'actions' => [
-    //             [
-    //                 'type' => 'message',
-    //                 'label' => 'ถูกต้อง',
-    //                 'text' => 'อายุครรภ์ถูกต้อง'
-    //             ],
-    //             [
-    //                 'type' => 'message',
-    //                 'label' => 'ไม่ถูกต้อง',
-    //                 'text' => 'ไม่ถูกต้อง'
-    //             ],
-    //         ]
-    //     ]
-    // ];   
+    $replyToken = $event['replyToken'];
+    $messages = [
+        'type' => 'template',
+        'altText' => 'this is a confirm template',
+        'template' => [
+            'type' => 'confirm',
+            'text' =>  $age_pre ,
+            'actions' => [
+                [
+                    'type' => 'message',
+                    'label' => 'ถูกต้อง',
+                    'text' => 'อายุครรภ์ถูกต้อง'
+                ],
+                [
+                    'type' => 'message',
+                    'label' => 'ไม่ถูกต้อง',
+                    'text' => 'ไม่ถูกต้อง'
+                ],
+            ]
+        ]
+    ];   
       $url = 'https://api.line.me/v2/bot/message/reply';
          $data = [
           'replyToken' => $replyToken,
