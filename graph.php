1a0593b09543d0cbc5567536ad013771ad24eb7e
piyawan <tho.piyawan@gmail.com>
2017-09-28 10:45:55 +0700

    s

--- graph.php
+++ graph.php
@@ -1,5 +1,8 @@
 
  <?php
+
+ $conn_string = "host=ec2-54-163-233-201.compute-1.amazonaws.com port=5432 dbname=dchdrsngrf50pd user=njppbbukwreesq password=c6b890bd6e0dccc4a5db3308869ba5e2735fe0e5df7a3f0de6f114cc24752e04";
+ $dbconn = pg_pconnect($conn_string);
 $user_id = $_GET["data"];
  // echo $user_id;
 
