9a26ed8f8b553628191dd29440a17ac234d57dd7
piyawan <tho.piyawan@gmail.com>
2017-09-28 16:52:59 +0700

    t

--- graph.php
+++ graph.php
@@ -113,7 +113,7 @@ var chart = AmCharts.makeChart("chartdiv", {
 chart.addListener("dataUpdated", zoomChart);
 
 function zoomChart() {
-    // chart.zoomToDates(new Date(2012, 0, 3), new Date(2012, 0, 11));
+    chart.zoomToDates(new Date(2012, 0, 3), new Date(2012, 0, 11));
 }
 
 // generate some random data, quite different range
