eb45bea0c8bce250d0c3534d69f3592da88f1ebc
piyawan <tho.piyawan@gmail.com>
2017-09-28 14:35:54 +0700

    s

--- graph.php
+++ graph.php
@@ -15,7 +15,7 @@ $user_id = pg_escape_string($user);
 $check = pg_query($dbconn,"SELECT user_weight FROM user_data  WHERE  user_id = '{$user_id}'  ");
                 while ($arr= pg_fetch_array($check)) {
               
-                  echo $arr0 = $arr[0];
+                 $arr0 = $arr[0];
          
                 } 
 
@@ -24,7 +24,7 @@ $check_q = pg_query($dbconn,"SELECT his_preg_week ,his_preg_weight FROM history_
                     // $i=0;
                    // echo $array_out[] =$arr;
                    // $i++;
-                  echo $arr0 = $arr[0];
+                  $arr0 = $arr[0];
                   echo $arr1 = $arr[1]-$arr0 ;
                   // echo $arr[$i++] ;
                   // var_dump($arr);
