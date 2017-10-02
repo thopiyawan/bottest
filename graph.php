ea626a847760b03296d05faa3d3a78004a9bd521
piyawan <tho.piyawan@gmail.com>
2017-10-02 13:23:05 +0700

    x

--- graph.php
+++ graph.php
@@ -30,7 +30,7 @@ $check_q = pg_query($dbconn,"SELECT his_preg_week ,his_preg_weight FROM history_
                                       'duration'=> $weight);
                 }   
 $data = json_encode($arrayName);
-echo $data ;
+$data ;
 echo "var data = '$data';";
 ?>
 <!DOCTYPE html>
@@ -145,40 +145,40 @@ function generateChartData() {
 //     echo '"'.$info['his_preg_weight'].'",'; 
 // ?>];
 
-//     var we = "<?php
+    var we = "<?php
 
-// $conn_string = "host=ec2-54-163-233-201.compute-1.amazonaws.com port=5432 dbname=dchdrsngrf50pd user=njppbbukwreesq password=c6b890bd6e0dccc4a5db3308869ba5e2735fe0e5df7a3f0de6f114cc24752e04";
-// $dbconn = pg_pconnect($conn_string);
-// if (!$dbconn) {
-//     die("Connection failed: " . mysqli_connect_error());
-// }
+$conn_string = "host=ec2-54-163-233-201.compute-1.amazonaws.com port=5432 dbname=dchdrsngrf50pd user=njppbbukwreesq password=c6b890bd6e0dccc4a5db3308869ba5e2735fe0e5df7a3f0de6f114cc24752e04";
+$dbconn = pg_pconnect($conn_string);
+if (!$dbconn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
 
-// $user = $_GET["data"];
-// $user_id = pg_escape_string($user);
-//  // echo $user_id;
+$user = $_GET["data"];
+$user_id = pg_escape_string($user);
+ // echo $user_id;
 
-// $check = pg_query($dbconn,"SELECT user_weight FROM user_data  WHERE  user_id = '{$user_id}'  ");
-//                 while ($row= pg_fetch_row($check)) {
+$check = pg_query($dbconn,"SELECT user_weight FROM user_data  WHERE  user_id = '{$user_id}'  ");
+                while ($row= pg_fetch_row($check)) {
               
-//                  $result = $row[0];
+                 $result = $row[0];
   
-//                 } 
+                } 
 
-// $a =[];
-// $arrayName=[];
-// $check_q = pg_query($dbconn,"SELECT his_preg_week ,his_preg_weight FROM history_preg  WHERE  user_id = '{$user_id}'  ");
-//                 while ($arr= pg_fetch_array($check_q)) {
-//                   $week = $arr[0];
-//                   $weight = $arr[1]-$result;
+$a =[];
+$arrayName=[];
+$check_q = pg_query($dbconn,"SELECT his_preg_week ,his_preg_weight FROM history_preg  WHERE  user_id = '{$user_id}'  ");
+                while ($arr= pg_fetch_array($check_q)) {
+                  $week = $arr[0];
+                  $weight = $arr[1]-$result;
          
-//                   $arrayName[] = array( 'date' => '2012-01-06',
-//                                         'duration'=> $weight);
-//                 }   
-// $b = json_encode($arrayName);
-// echo $b ;
-// echo "var data = '$data';";
-// ?>";
+                  $arrayName[] = array( 'date' => '2012-01-06',
+                                        'duration'=> $weight);
+                }   
+$b = json_encode($arrayName);
+echo $b ;
+echo "var data = '$data';";
+?>";
       var chartData = [];
           chartData.push(data);    
     
