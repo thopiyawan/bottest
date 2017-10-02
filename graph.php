685d6384bd0d8990c5e8668c33d6bc8d4c56d6ff
piyawan <tho.piyawan@gmail.com>
2017-10-02 16:06:55 +0700

    d

--- graph.php
+++ graph.php
@@ -26,7 +26,7 @@ $check_q = pg_query($dbconn,"SELECT his_preg_week ,his_preg_weight FROM history_
                   $week = $arr[0];
                   $weight = $arr[1]-$result;
          
-                  $arrayName[] = array( 'date' => '2012-01-06',
+                  $arrayName[] = array( 'date' => $week ,
                                       'duration'=> $weight);
                 }   
 $data = json_encode($arrayName);
