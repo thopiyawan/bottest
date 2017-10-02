25b8578d416f5bd19bfe176f7c052fece25255f4
piyawan <tho.piyawan@gmail.com>
2017-10-02 11:14:28 +0700

    ด

--- graph.php
+++ graph.php
@@ -33,5 +33,152 @@ array_push($a,$arrayName);
 
 print_r($a);
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<script src="https://www.amcharts.com/lib/3/amcharts.js"></script>
+<script src="https://www.amcharts.com/lib/3/serial.js"></script>
+<script src="https://www.amcharts.com/lib/3/plugins/export/export.min.js"></script>
+<link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
+<script src="https://www.amcharts.com/lib/3/themes/light.js"></script>
+<style>
+#chartdiv {
+  width   : 100%;
+  height    : 500px;
+  font-size : 11px;
+}                            
+</style>
+<script type="text/javascript">
+    
+var chartData = generateChartData();
+
+var chart = AmCharts.makeChart("chartdiv", {
+    "type": "serial",
+    "theme": "light",
+    "marginRight": 80,
+    "dataProvider": chartData,
+    "balloon": {
+        "cornerRadius": 6,
+        "horizontalPadding": 15,
+        "verticalPadding": 10
+    },
+    "valueAxes": [{
+        "position": "left",
+        "title": "weight"
+    }],
+    "graphs": [{
+        "bullet": "square",
+        "bulletBorderAlpha": 1,
+        "bulletBorderThickness": 1,
+        "fillAlphas": 0.3,
+        "fillColorsField": "lineColor",
+        "legendValueText": "[[value]]",
+        "lineColorField": "lineColor",
+        "title": "duration",
+        "valueField": "duration"
+    }],
+    "chartScrollbar": {
+
+    },
+    "chartCursor": {
+        "categoryBalloonDateFormat": "YYYY MMM DD",
+        "cursorAlpha": 0,
+        "fullWidth": true
+    },
+    "dataDateFormat": "YYYY-MM-DD",
+    "categoryField": "date",
+    "categoryAxis": {
+        "dateFormats": [{
+            "period": "DD",
+            "format": "DD"
+        }, {
+            "period": "WW",
+            "format": "MMM DD"
+        }, {
+            "period": "MM",
+            "format": "MMM"
+        }, {
+            "period": "YYYY",
+            "format": "YYYY"
+        }],
+        "parseDates": true,
+        "autoGridCount": false,
+        "axisColor": "#555555",
+        "gridAlpha": 0,
+        "gridCount": 50
+    },
+    "export": {
+        "enabled": true
+    }
+});
+
+
+
+chart.addListener("dataUpdated", zoomChart);
+
+function zoomChart() {
+    // chart.zoomToDates(new Date(2012, 0, 3), new Date(2012, 0, 11));
+}
+
+// generate some random data, quite different range
+function generateChartData() {
+
+
+var c =[<?php 
+$data= [];
+while($info = pg_fetch_array($check_q))
+    $a =  $info['his_preg_week'].','; 
+   $arrayg = array('date' => '2012-01-01' ,
+                  'duration'=> '255' );
+  array_push($data,$arrayg); 
+  echo $data;
+?>];
+<?php
+$data=pg_query($dbconn,"SELECT his_preg_week ,his_preg_weight FROM history_preg  WHERE  user_id = '{$user_id}'  ");
+
+echo myData;
+?>
+var myLabels=[<?php 
+while($info = pg_fetch_array($data))
+    echo '"'.$info['his_preg_weight'].'",'; 
+?>];
+
+    var we = "<?php 
+          $a =[];
+   $arrayName=[];
+$check_q = pg_query($dbconn,"SELECT his_preg_week ,his_preg_weight FROM history_preg  WHERE  user_id = '{$user_id}'  ");
+                while ($arr= pg_fetch_array($check_q)) {
+                  $week = $arr[0];
+                  $weight = $arr[1]-$result;
+         
+                  $arrayName[] = array( 'date' => $week,
+                                        'duration'=> $weight);
+                }  
+array_push($a,$arrayName);  
+?>";
+     
+
+
+
+
+      var chartData = [];
+        chartData.push(we);    
+    
+    return chartData;
+}
+</script>
+
+<body>
+
+<div id="chartdiv"></div>
+<!-- <?php echo $arr0; ?> -->
+</body>
+</html>
+
+
+
+
 
 
