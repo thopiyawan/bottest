74c36fa382ed1a36fa3bdb104be142782541f6e4
piyawan <tho.piyawan@gmail.com>
2017-10-09 13:47:32 +0700

    d

--- autoreply.php
+++ autoreply.php
@@ -44,39 +44,38 @@ $arrlength = count($s);
 for($x = 0; $x < $arrlength+1 ; $x++) {
        $userid = $s[0][$x];
        $check_q = pg_query($dbconn,"SELECT preg_week FROM user_data WHERE user_id = '{$userid}'  ");
-
             while ($row = pg_fetch_row($check_q)) {
-                  $p_week =  $row['preg_week'];
+                echo  $p_week =  $row['preg_week'];
                 } 
  
-        $messages = [
-                        'type' => 'text',
-                        'text' => 'สัปดาห์นี้คุณมีน้ำหนักเท่าไรคะ'
-                    ];
-         $messages1 = [
-                        'type' => 'text',
-                        'text' => 'hi'.$p_week
-                    ];
+        // $messages = [
+        //                 'type' => 'text',
+        //                 'text' => 'สัปดาห์นี้คุณมีน้ำหนักเท่าไรคะ'
+        //             ];
+        //  $messages1 = [
+        //                 'type' => 'text',
+        //                 'text' => 'hi'.$p_week
+        //             ];
 
 
 
-         $url = 'https://api.line.me/v2/bot/message/push';
-         $data = [
-          'to' => $userid ,
-          'messages' => [$messages,$messages1],
-         ];
-         error_log(json_encode($data));
-         $post = json_encode($data);
-         $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-         $ch = curl_init($url);
-         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-         curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-         $result = curl_exec($ch);
-         curl_close($ch);
-         echo $result . "\r\n";
+         // $url = 'https://api.line.me/v2/bot/message/push';
+         // $data = [
+         //  'to' => $userid ,
+         //  'messages' => [$messages,$messages1],
+         // ];
+         // error_log(json_encode($data));
+         // $post = json_encode($data);
+         // $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+         // $ch = curl_init($url);
+         // curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+         // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+         // curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+         // curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+         // curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+         // $result = curl_exec($ch);
+         // curl_close($ch);
+         // echo $result . "\r\n";
 
 
 
