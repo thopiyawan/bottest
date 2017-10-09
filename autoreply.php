aca28b42e98a4bdc83f5ba1fe82c3c66142ae8b7
piyawan <tho.piyawan@gmail.com>
2017-10-09 09:53:54 +0700

    g

--- autoreply.php
+++ autoreply.php
@@ -35,15 +35,15 @@ $user_id = pg_escape_string($user);
        $h = date("H:i");
 //****************ทดสอบ จบ
 
-
+$seqcode =[];
 $check_q = pg_query($dbconn,"SELECT user_id FROM user_data   ");
 
             while ($row = pg_fetch_assoc($check_q)) {
                   $seqcode[] =  $row[0];
         
                 } 
-
- echo $seqcode;
+        array_push( $s,$seqcode)
+ echo $s;
 
 
 //*********//
