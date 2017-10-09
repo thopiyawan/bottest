f1be449c3e95598954b19de36f57db8b90d6a28f
piyawan <tho.piyawan@gmail.com>
2017-10-09 09:07:21 +0700

    d

--- autoreply.php
+++ autoreply.php
@@ -3,25 +3,29 @@
 //query user id from userdata
 
 //database
-// $conn_string = "host=ec2-54-163-233-201.compute-1.amazonaws.com port=5432 dbname=dchdrsngrf50pd user=njppbbukwreesq password=c6b890bd6e0dccc4a5db3308869ba5e2735fe0e5df7a3f0de6f114cc24752e04";
-// $dbconn = pg_pconnect($conn_string);
-// //
+$conn_string = "host=ec2-54-163-233-201.compute-1.amazonaws.com port=5432 dbname=dchdrsngrf50pd user=njppbbukwreesq password=c6b890bd6e0dccc4a5db3308869ba5e2735fe0e5df7a3f0de6f114cc24752e04";
+$dbconn = pg_pconnect($conn_string);
+//
 
 $access_token = 'GKg1wAZ/gjMr6yh3dGmPjuq8HnkDQEZsOdPEfyur3h7JmjdT2JihbEBHL6S4BrLnHCuu0Cv2fSbvwv0/xZqYw+TEjmmqW2mjC5NB9BcVGguZq3CIHX+Vt+fvPcNwtcT2ER0LLVXSwhNN4aVJT0Q08QdB04t89/1O/w1cDnyilFU=';
 
-// $content = file_get_contents('php://input');
-// // Parse JSON
-// $events = json_decode($content, true);
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+
+$types = array();
+
+while(($row =  mysql_fetch_assoc($result))) {
+    $types[] = $row['type'];
+}
+
+$curr_years = date("Y");
+$curr_y = ($curr_years+ 543);
 
 
-
-// $curr_years = date("Y");
-// $curr_y = ($curr_years+ 543);
-
-
-// $_msg = $events['events'][0]['message']['text'];
-// $user = $events['events'][0]['source']['userId'];
-// $user_id = pg_escape_string($user);
+$_msg = $events['events'][0]['message']['text'];
+$user = $events['events'][0]['source']['userId'];
+$user_id = pg_escape_string($user);
 
 
 
@@ -30,77 +34,72 @@ $access_token = 'GKg1wAZ/gjMr6yh3dGmPjuq8HnkDQEZsOdPEfyur3h7JmjdT2JihbEBHL6S4BrL
 //                   echo $seqcode =  $row[0];
 //                   echo $sender = $row[2]; 
 //                 } 
-// //****************ทดสอบ
-//        $d = date("D");
-//        $h = date("H:i");
-// //****************ทดสอบ จบ
+//****************ทดสอบ
+       $d = date("D");
+       $h = date("H:i");
+//****************ทดสอบ จบ
+
+
+$check_q = pg_query($dbconn,"SELECT sender_id FROM user_data   ");
+
+$types = array();
+
+while(($row =  pg_fetch_assoc($result))) {
+    $types[] = $row['type'];
+}]
+echo $types;
 
 
 
 
-// //*********//
-// //query data , 
+//*********//
+//query data , 
 
  
 
-//     $check_q = pg_query($dbconn,"SELECT  user_age, user_weight ,user_height ,preg_week  FROM user_data WHERE  user_id = '{$user_id}' ");
+    // $check_q = pg_query($dbconn,"SELECT  user_age, user_weight ,user_height ,preg_week  FROM user_data WHERE  user_id = '{$user_id}' ");
 
-//                 while ($row = pg_fetch_row($check_q)) {
-//                   echo $answer1 = $row[0]; 
-//                   echo $weight = $row[1]; 
-//                   echo $height = $row[2]; 
-//                   echo $answer4 = $row[3]+1;  
-//                 } 
+    //             while ($row = pg_fetch_row($check_q)) {
+    //               echo $answer1 = $row[0]; 
+    //               echo $weight = $row[1]; 
+    //               echo $height = $row[2]; 
+    //               echo $answer4 = $row[3]+1;  
+    //             } 
           
 
-//                     $replyToken = $event['replyToken'];
-//                     $messages2 = [
-//                         'type' => 'text',
-//                         'text' => 'คุณมีอายุครรภ์'.$answer4.'สัปดาห์'
-//                       ];
+    //                 $replyToken = $event['replyToken'];
+    //                 $messages2 = [
+    //                     'type' => 'text',
+    //                     'text' => 'คุณมีอายุครรภ์'.$answer4.'สัปดาห์'
+    //                   ];
     
-//                     $messages3 = [
-                                                              
-//                                   'type' => 'template',
-//                                   'altText' => 'template',
-//                                   'template' => [
-//                                       'type' => 'buttons',
-//                                       'thumbnailImageUrl' => 'https://bottest14.herokuapp.com/week/'.$answer4 .'.jpg',
-//                                       'title' => 'ลูกน้อยของคุณ',
-//                                       'text' =>  'อายุ'.$answer4.'สัปดาห์',
-//                                       'actions' => [
-//                                           // [
-//                                           //     'type' => 'postback',
-//                                           //     'label' => 'good',
-//                                           //     'data' => 'value'
-//                                           // ],
-//                                           [
-//                                               'type' => 'uri',
-//                                               'label' => 'กราฟ',
-//                                               'uri' => 'https://bottest14.herokuapp.com/graph.php?data='.$user_id
-//                                           ]
-//                                       ]
-//                                   ]
-//                               ];
-//          $q1 = pg_exec($dbconn, "UPDATE user_data SET user_weight= $answer4 WHERE user_id = '{$user_id}' ") or die(pg_errormessage()); 
-//          $des_preg = pg_query($dbconn,"SELECT  descript,img FROM pregnants WHERE  week = $answer4  ");
-//               while ($row = pg_fetch_row($des_preg)) {
-//                   echo $des = $row[0]; 
-//                   echo $img = $row[1]; 
- 
-//                 } 
-//                     $messages4 = [
-//                         'type' => 'text',
-//                         'text' =>  $des
-//                       ];
-            $messages = [
-                        'type' => 'text',
-                        'text' => 'สัปดาห์นี้คุณมีน้ำหนักเท่าไรคะ'
-                      ];
+    //                 $messages3 =          [
+    //                     'type' => 'image',
+    //                     'originalContentUrl'=> 'https://bottest14.herokuapp.com/week/'.$answer4 .'.jpg',
+    //                     'previewImageUrl'=> 'https://bottest14.herokuapp.com/week/'];
 
+    //      $q1 = pg_exec($dbconn, "UPDATE user_data SET user_weight= $answer4 WHERE user_id = '{$user_id}' ") or die(pg_errormessage()); 
+    //      $des_preg = pg_query($dbconn,"SELECT  descript,img FROM pregnants WHERE  week = $answer4  ");
+    //           while ($row = pg_fetch_row($des_preg)) {
+    //               echo $des = $row[0]; 
+    //               echo $img = $row[1]; 
+ 
+    //             } 
+    //                 $messages4 = [
+    //                     'type' => 'text',
+    //                     'text' =>  $des
+    //                   ];
+    //         $messages = [
+    //                     'type' => 'text',
+    //                     'text' => 'สัปดาห์นี้คุณมีน้ำหนักเท่าไรคะ'
+    //                   ];
+
+       
+
+             
          $url = 'https://api.line.me/v2/bot/message/push';
          $data = [
-          'to' => 'U2dc636d2cd052e82c29f5284e00f69b9',
+          'to' => $userid ,
           'messages' => [$messages],
          ];
          error_log(json_encode($data));
