1f5546f00df2419c45a907030f15bf3ecc3ae380
piyawan <tho.piyawan@gmail.com>
2017-10-09 09:59:55 +0700

    s

--- autoreply.php
+++ autoreply.php
@@ -40,7 +40,7 @@ $s =[];
 $check_q = pg_query($dbconn,"SELECT user_id FROM user_data   ");
 
             while ($row = pg_fetch_assoc($check_q)) {
-                  $seqcode[] =  $row[0];
+                  $seqcode[] =  $row['user_id'];
         
                 } 
         array_push( $s,$seqcode);
