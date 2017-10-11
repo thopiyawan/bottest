13fd7dfd2ec62aa37f16d7722fbe7340eb4a8496
piyawan <tho.piyawan@gmail.com>
2017-10-11 15:34:38 +0700

    h

--- index.php
+++ index.php
@@ -587,8 +587,12 @@ if (!is_null($events['events'])) {
                         'type' => 'text',
                         'text' => 'ดึงกราฟ' 
                       ];  
-
-    $q = pg_exec($dbconn, "UPDATE recordofpregnancy SET preg_weight = $answer WHERE user_id = '{$user_id}' ORDER BY updated_at DESC LIMIT 1 ") or die(pg_errormessage());  
+    $check = pg_query($dbconn,"SELECT preg_week FROM users WHERE user_id = '{$user_id}'  ");
+            while ($row = pg_fetch_row($check)) {
+                echo  $p_week =  $row[0]+1;
+                } 
+    // $q = pg_exec($dbconn, "UPDATE recordofpregnancy SET preg_weight = $answer WHERE user_id = '{$user_id}' ") or die(pg_errormessage());  
+    $q2 = pg_exec($dbconn, "INSERT INTO recordofpregnancy(user_id, preg_week, preg_weight,updated_at )VALUES('{$user_id}',$p_week,$answer ,  NOW()) ") or die(pg_errormessage());  
 
     $q2 = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0000', $weight,'','0',NOW(),NOW())") or die(pg_errormessage()); 
 
