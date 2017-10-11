c0896af9d42dff3429cbdd451bc302bbab3ba809
piyawan <tho.piyawan@gmail.com>
2017-10-11 13:11:08 +0700

    d

--- autoreply.php
+++ autoreply.php
@@ -30,7 +30,7 @@ $user_id = pg_escape_string($user);
 
 $seqcode =[];
 $s =[];
-$check_q = pg_query($dbconn,"SELECT DISTINCT user_id FROM user_data   ");
+$check_q = pg_query($dbconn,"SELECT DISTINCT user_id FROM users   ");
 
             while ($row = pg_fetch_assoc($check_q)) {
                   $seqcode[] =  $row['user_id'];
@@ -100,7 +100,7 @@ for($x = 0; $x < $arrlength+1 ; $x++) {
                      ];
 
         $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0017','','0000','0',NOW(),NOW())") or die(pg_errormessage()); 
-        $q2 = pg_exec($dbconn, "INSERT INTO recordofpregnancy(user_id, preg_week, preg_weight)VALUES('{$user_id}',$p_week,'0') ") or die(pg_errormessage());        
+        $q2 = pg_exec($dbconn, "INSERT INTO recordofpregnancy(user_id, preg_week, preg_weight,updated_at )VALUES('{$user_id}',$p_week,'0',  NOW()) ") or die(pg_errormessage());        
              
          $url = 'https://api.line.me/v2/bot/message/push';
          $data = [
