34b30c7fad553f7aceb845567aa518849621419e
piyawan <tho.piyawan@gmail.com>
2017-10-11 14:26:20 +0700

    d

--- autoreply.php
+++ autoreply.php
@@ -37,8 +37,8 @@ print_r($s);
 $arrlength = count($s);
 
 for($x = 0; $x < $arrlength+1 ; $x++) {
-       $userid = $s[0][$x];
-       $user_id = pg_escape_string($userid);
+       $user_id = $s[0][$x];
+    
        $check = pg_query($dbconn,"SELECT preg_week FROM users WHERE user_id = '{$user_id}'  ");
             while ($row = pg_fetch_row($check)) {
                 echo  $p_week =  $row[0]+1;
@@ -71,53 +71,52 @@ for($x = 0; $x < $arrlength+1 ; $x++) {
 
         }else{
  
-        $messages1 = [
-                        'type' => 'text',
-                        'text' => 'สัปดาห์นี้คุณมีอายุครรภ์'.$p_week.'สัปดาห์แล้วนะคะ'
-                     ];
-        $messages2 = [ 'type'=> 'image',
-                       'originalContentUrl'=> 'https://bottest14.herokuapp.com/week/'.$p_week.'.jpg',
-                       'previewImageUrl'=> 'https://bottest14.herokuapp.com/week/'.$p_week.'.jpg'
-                     ];
-      
-        $des_preg = pg_query($dbconn,"SELECT  descript FROM pregnants WHERE  week = $p_week   ");
-              while ($row = pg_fetch_row($des_preg)) {
-                  echo $des = $row[0];
- 
-                } 
-        $messages3 = [
-                        'type' => 'text',
-                        'text' =>  $des
-                     ];
-        $messages4 = [
-                        'type' => 'text',
-                        'text' => 'สัปดาห์นี้คุณแม่มีน้ำหนักเท่าไรแล้วคะ?'
-                     ];
+                $messages1 = [
+                                'type' => 'text',
+                                'text' => 'สัปดาห์นี้คุณมีอายุครรภ์'.$p_week.'สัปดาห์แล้วนะคะ'
+                             ];
+                $messages2 = [ 'type'=> 'image',
+                               'originalContentUrl'=> 'https://bottest14.herokuapp.com/week/'.$p_week.'.jpg',
+                               'previewImageUrl'=> 'https://bottest14.herokuapp.com/week/'.$p_week.'.jpg'
+                             ];
+              
+                $des_preg = pg_query($dbconn,"SELECT  descript FROM pregnants WHERE  week = $p_week   ");
+                      while ($row = pg_fetch_row($des_preg)) {
+                          echo $des = $row[0];
+         
+                        } 
+                $messages3 = [
+                                'type' => 'text',
+                                'text' =>  $des
+                             ];
+                $messages4 = [
+                                'type' => 'text',
+                                'text' => 'สัปดาห์นี้คุณแม่มีน้ำหนักเท่าไรแล้วคะ?'
+                             ];
 
+                $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0017','','0000','0',NOW(),NOW())") or die(pg_errormessage()); 
 
+                $q2 = pg_exec($dbconn, "INSERT INTO recordofpregnancy(user_id, preg_week, preg_weight,updated_at )VALUES('{$user_id}',$p_week,'0',  NOW()) ") or die(pg_errormessage());        
+                     
+                 $url = 'https://api.line.me/v2/bot/message/push';
+                 $data = [
+                  'to' => $userid ,
+                  'messages' => [$messages1,$messages2,$messages3,$messages4],
+                 ];
+                 error_log(json_encode($data));
+                 $post = json_encode($data);
+                 $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+                 $ch = curl_init($url);
+                 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                 curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+                 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+                 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+                 $result = curl_exec($ch);
+                 curl_close($ch);
+                 echo $result . "\r\n";
 
         }
-         $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0017','','0000','0',NOW(),NOW())") or die(pg_errormessage()); 
-        
-        $q2 = pg_exec($dbconn, "INSERT INTO recordofpregnancy(user_id, preg_week, preg_weight,updated_at )VALUES('{$user_id}',$p_week,'0',  NOW()) ") or die(pg_errormessage());        
-             
-         $url = 'https://api.line.me/v2/bot/message/push';
-         $data = [
-          'to' => $userid ,
-          'messages' => [$messages1,$messages2,$messages3,$messages4],
-         ];
-         error_log(json_encode($data));
-         $post = json_encode($data);
-         $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-         $ch = curl_init($url);
-         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-         curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-         $result = curl_exec($ch);
-         curl_close($ch);
-         echo $result . "\r\n";
 
 }
 
