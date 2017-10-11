aeee352f57f803b54c17da91356261eaa6dfe5e4
piyawan <tho.piyawan@gmail.com>
2017-10-11 15:58:24 +0700

    f

--- index.php
+++ index.php
@@ -576,14 +576,14 @@ if (!is_null($events['events'])) {
 
 }elseif ($event['message']['text'] == "น้ำหนักถูกต้อง" ) {
 
-        $check_q = pg_query($dbconn,"SELECT seqcode, sender_id ,updated_at ,answer FROM sequentsteps  WHERE sender_id = '{$user_id}' order by updated_at desc limit 1   ");
+    $check_q = pg_query($dbconn,"SELECT seqcode, sender_id ,updated_at ,answer FROM sequentsteps  WHERE sender_id = '{$user_id}' order by updated_at desc limit 1   ");
 
                 while ($row = pg_fetch_row($check_q)) {
             
                   echo $answer = $row[3];  
                 } 
              
-        $check = pg_query($dbconn,"SELECT preg_week FROM recordofpregnancy WHERE user_id = '{$user_id}' order by updated_at desc limit 1 ");
+    $check = pg_query($dbconn,"SELECT preg_week FROM recordofpregnancy WHERE user_id = '{$user_id}' order by updated_at desc limit 1 ");
             while ($row = pg_fetch_row($check)) {
                 echo  $p_week =  $row[0]+1;
                 } 
@@ -592,12 +592,12 @@ if (!is_null($events['events'])) {
 
     $q2 = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0000', $weight,'','0',NOW(),NOW())") or die(pg_errormessage()); 
 
-
-        $replyToken = $event['replyToken'];
+   $replyToken = $event['replyToken'];
                  $messages = [
                         'type' => 'text',
-                        'text' => 'ดึงกราฟ' 
+                        'text' => 'fgsbfgrsbfbfgrabgfbgfs gf'
                       ];  
+
 
   }elseif (is_numeric($_msg) !== false && $seqcode == "0017"  )  {
                  $weight =  $_msg;
