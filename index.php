8b3a8f65e439f56d29492adbe4c60d46fc557aa3
piyawan <tho.piyawan@gmail.com>
2017-10-12 09:15:10 +0700

    f

--- index.php
+++ index.php
@@ -164,7 +164,7 @@ if (!is_null($events['events'])) {
     $m = $month_today- $month;
     $m1 = abs($m);
     if (is_numeric($month) !== false &&is_numeric($date) !== false && strlen($date) == 2 && strlen($month) == 2  ) {
-        if($date<31 && $date >=0 && $month <12 && $month>=0 && $month<$month_today || ( $month== $month_today &&  $date< $date_today )){
+        if($date<31 && $date >=0 && $month <12 && $month>=0 && $month<$month_today && $m1<=9 || ( $month== $month_today &&  $date< $date_today )){
            if($date>$date_today){
                $d_pre = $date - $date_today;
                 if($d_pre>=7){
