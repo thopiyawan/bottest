510ad7480b4e745879135797efc2a61cfec13163
piyawan <tho.piyawan@gmail.com>
2017-10-31 10:32:23 +0700

    update

--- index.php
+++ index.php
@@ -613,65 +613,61 @@ $replyToken = $event['replyToken'];
    //      ]
    //  ];
 
-// $messages = [
-//   'type'=> 'template',
-//   'altText'=> 'this is a carousel template',
-//   'template'=> [
-//       'type'=> 'carousel',
-//       'columns'=> [
-//           [
-//             'thumbnailImageUrl'=> 'https://example.com/bot/images/item1.jpg',
-//             'title'=> 'this is menu',
-//             'text'=> 'description',
-//             'actions'=> [
-//                 [
-//                     'type'=> 'postback',
-//                     'label'=> 'Buy',
-//                     'data'=> 'action=buy&itemid=111'
-//                 ],
-//                 [
-//                     'type'=> 'postback',
-//                     'label'=> 'Add to cart',
-//                    'data'=> 'action=add&itemid=111'
-//                 ],
-//                 [
-//                     'type'=> 'uri',
-//                     'label'=> 'View detail',
-//                     'uri'=> 'http://example.com/page/111'
-//                 ]
-//             ]
-//           ],
-//           [
-//             [
-//             'thumbnailImageUrl'=> 'https://example.com/bot/images/item1.jpg',
-//             'title'=> 'this is menu',
-//             'text'=> 'description',
-//             'actions'=> [
-//                 [
-//                     'type'=> 'postback',
-//                     'label'=> 'Buy',
-//                     'data'=> 'action=buy&itemid=111'
-//                 ],
-//                 [
-//                     'type'=> 'postback',
-//                     'label'=> 'Add to cart',
-//                    'data'=> 'action=add&itemid=111'
-//                 ],
-//                 [
-//                     'type'=> 'uri',
-//                     'label'=> 'View detail',
-//                     'uri'=> 'http://example.com/page/111'
-//                 ]
-//             ]
-//           ]
-//       ]
-//   ]
-// ]
-// ];
 $messages = [
-          'type' => 'text',
-          'text' => 'แพ้ท้องเหรอออ'
-        ]; 
+  'type'=> 'template',
+  'altText'=> 'this is a carousel template',
+  'template'=> [
+      'type'=> 'carousel',
+      'columns'=> [
+          [
+            'thumbnailImageUrl'=> 'https://example.com/bot/images/item1.jpg',
+            'title'=> 'this is menu',
+            'text'=> 'description',
+            'actions'=> [
+                [
+                    'type'=> 'postback',
+                    'label'=> 'Buy',
+                    'data'=> 'action=buy&itemid=111'
+                ],
+                [
+                    'type'=> 'postback',
+                    'label'=> 'Add to cart',
+                   'data'=> 'action=add&itemid=111'
+                ],
+                [
+                    'type'=> 'uri',
+                    'label'=> 'View detail',
+                    'uri'=> 'http://example.com/page/111'
+                ]
+            ]
+          ],
+          [
+            'thumbnailImageUrl'=> 'https://example.com/bot/images/item1.jpg',
+            'title'=> 'this is menu',
+            'text'=> 'description',
+            'actions'=> [
+                [
+                    'type'=> 'postback',
+                    'label'=> 'Buy',
+                    'data'=> 'action=buy&itemid=111'
+                ],
+                [
+                    'type'=> 'postback',
+                    'label'=> 'Add to cart',
+                   'data'=> 'action=add&itemid=111'
+                ],
+                [
+                    'type'=> 'uri',
+                    'label'=> 'View detail',
+                    'uri'=> 'http://example.com/page/111'
+                ]
+            ]
+          ]
+      ]
+  ]
+
+];
+
 
 
 }elseif($event['message']['text'] == "Clear" ){
