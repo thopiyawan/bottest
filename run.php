c97f83a8eba221c415118f5cd8fbce5ab8d7d01c
piyawan <tho.piyawan@gmail.com>
2017-10-31 10:49:31 +0700

    e

--- run.php
+++ run.php
@@ -7,9 +7,9 @@
     $x_tra = 'แพ้ท้อง';
     $url = 'https://www.googleapis.com/customsearch/v1?&cx=014388729015054466439:e_gyj6qnxr8&key=AIzaSyDmVU8aawr5mNpqbiUdYMph8r7K-siKn-0&q='.$x_tra;
     // $url2 = 'https://www.googleapis.com/customsearch/v1?&cx=014388729015054466439:gqr4m9bfx0i&key=AIzaSyDmVU8aawr5mNpqbiUdYMph8r7K-siKn-0&q='.$x_tra;
-    $json1= file_get_contents($url);
+    $json= file_get_contents($url);
     // $json2= file_get_contents($url2);
-    var_dump($json1);
+    // var_dump($json1);
     
     $events = json_decode($json, true);
     $title= $events['items'][0]['title'];
