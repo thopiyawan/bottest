4dc27556ec351c57b9554fa1ff8c204e22a48282
piyawan <tho.piyawan@gmail.com>
2017-10-31 11:07:36 +0700

    f

--- index.php
+++ index.php
@@ -572,7 +572,7 @@ $replyToken = $event['replyToken'];
     $replyToken = $event['replyToken'];
     // $x_tra = str_replace("","", $_msg);
     $_msg ='แพ้ท้อง';
-    $url = 'https://www.googleapis.com/customsearch/v1?&cx=014388729015054466439:e_gyj6qnxr8&key=AIzaSyDmVU8aawr5mNpqbiUdYMph8r7K-siKn-0&q='.$_msg;
+    $url = 'https://www.googleapis.com/customsearch/v1?&cx=011030528095328264272:_0c9oat4ztq&key=AIzaSyBgzyv2TiMpaZxxthxX1jYNdskfxi7ah_4&q='.$_msg;
     // $url2 = 'https://www.googleapis.com/customsearch/v1?&cx=014388729015054466439:gqr4m9bfx0i&key=AIzaSyDmVU8aawr5mNpqbiUdYMph8r7K-siKn-0&q='.$x_tra;
     $json= file_get_contents($url);
     // $json= file_get_contents($url2);
