5b4dd052f6d261cab10d19d9bf6dbafdad2e00f3
piyawan <tho.piyawan@gmail.com>
2017-11-10 09:28:53 +0700

    x

--- run.php
+++ run.php
@@ -29,21 +29,6 @@ $objResult = pg_fetch_array($objQuery);
 echo json_encode($objResult);
 
 
-$ch = curl_init();
-// Disable SSL verification
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-// Will return the response, if false it print the response
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// Set the url
-curl_setopt($ch, CURLOPT_URL,"http://mikrotik108.com/api.php?id=U2dc636d2cd052e82c29f5284e00f69b9" );
-// Execute
-$result=curl_exec($ch);
-// Closing
-curl_close($ch);
-// แปลงข้อมูลที่รับมาในรูป json มาเป็น array จะได้ใช้ง่าย ๆ
-$DATA= json_decode($result, true);
-// //dump ข้อมูลออกมาดู
-print_r($DATA);
 
 
 //   $replyToken = $event['replyToken'];
